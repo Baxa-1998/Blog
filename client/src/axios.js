4406fc113c728524b16bacb751b67cbb63f58eb7
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-01-22 17:41:59 +0300

    eee

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({ 
   //  prod url 'https://blog-sever-ezqp.onrender.com'
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://blog-sever-ezqp.onrender.com',
 })
 instance.interceptors.request.use((config)=>{
   config.headers.Authorization = window.localStorage.getItem('token')
